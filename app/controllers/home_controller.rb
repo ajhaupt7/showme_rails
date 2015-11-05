f1d342014b65a17f2ec83ca9b66e2304d5317edd
Andrew Haupt & Tim Kellogg <student@epicodus.com>
2015-11-05 15:53:34 -0800

    fixed many a bug

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -5,17 +5,12 @@ class HomeController < ApplicationController
   end
 
   def spotify
-    year = params[:dateYear]
-    month = params[:dateMonth]
-    day = params[:dateDay]
-    if day.to_i < 10
-      day = '0' + day.to_s
-    end
-    params[:date] = "#{year}-#{month}-#{day}"
-    @events = search_bandsintown(params[:date], params[:city], params[:state])
-    respond_to do |format|
-      format.json { render json: @events }
-    end
+    Timeout::timeout(15) {
+      @events = search_bandsintown(params[:start_date], params[:end_date], params[:city], params[:state])
+      respond_to do |format|
+        format.json { render json: @events }
+      end
+    }
   end
 
 end
--- lib/api.rb
+++ lib/api.rb
@@ -3,89 +3,13 @@ require 'rest_client'
 
 
 module Api
-
-  # def return_similar(artist, city, state)
-  #   upcoming_events = []
-  #   results = []
-  #   results.push(get_more(artist))
-  #   random_num = Random.rand(results[0].length - 1)
-  #   results.push(search_spotify(artist))
-  #   results.flatten!
-  #   until results.length > 40 do
-  #     results.push(get_more(results[random_num].name))
-  #   end
-  #   results.flatten!
-  #   concert_artists = results.uniq { |result| result.name }
-  #
-  #   concert_artists.each do |artist|
-  #     concert = search_bandsintown(artist.name, city, state)
-  #     if concert != nil
-  #       upcoming_events.push(search_bandsintown(artist.name, city, state))
-  #     end
-  #   end
-  #
-  #   return upcoming_events
-  # end
-  #
-  #
-  # def search_spotify(query)
-  #   artists = RSpotify::Artist.search(query)
-  #   found_artist = artists.first
-  #   related_artists = found_artist.related_artists
-  #   least_popular = nil
-  #
-  #   related_artists.each do |related_artist|
-  #     if least_popular == nil
-  #       least_popular = related_artist
-  #     elsif related_artist.popularity < least_popular.popularity
-  #       least_popular = related_artist
-  #     end
-  #   end
-  #
-  #   if least_popular.popularity > 10
-  #     search_spotify(least_popular.name)
-  #   else
-  #     return related_artists
-  #   end
-  # end
-  #
-  # def get_more(query)
-  #   artists = RSpotify::Artist.search(query)
-  #   found_artist = artists.first
-  #   related_artists = found_artist.related_artists
-  # end
-  #
-  # def search_bandsintown(artist, city, state)
-  #   artist = artist.tr(
-  #   "ÀÁÂÃÄÅàáâãäåĀāĂăĄąÇçĆćĈĉĊċČčÐðĎďĐđÈÉÊËèéêëĒēĔĕĖėĘęĚěĜĝĞğĠġĢģĤĥĦħÌÍÎÏìíîïĨĩĪīĬĭĮįİıĴĵĶķĸĹĺĻļĽľĿŀŁłÑñŃńŅņŇňŉŊŋÒÓÔÕÖØòóôõöøŌōŎŏŐőŔŕŖŗŘřŚśŜŝŞşŠšſŢţŤťŦŧÙÚÛÜùúûüŨũŪūŬŭŮůŰűŲųŴŵÝýÿŶŷŸŹźŻżŽž",
-  #   "AAAAAAaaaaaaAaAaAaCcCcCcCcCcDdDdDdEEEEeeeeEeEeEeEeEeGgGgGgGgHhHhIIIIiiiiIiIiIiIiIiJjKkkLlLlLlLlLlNnNnNnNnnNnOOOOOOooooooOoOoOoRrRrRrSsSsSsSssTtTtTtUUUUuuuuUuUuUuUuUuUuWwYyyYyYZzZzZz")
-  #   begin
-  #     base_url = "http://api.bandsintown.com/events/search?artists[]=#{artist}&location=#{city},#{state}&radius=25&format=json&app_id=#{ENV['BANDSINTOWN_ID']}"
-  #     unclean = RestClient.get(base_url)
-  #     events = JSON.parse(unclean)
-  #     puts "#{artist} - #{events}"
-  #     return events
-  #   rescue RestClient::ResourceNotFound => e
-  #     puts "No events for #{artist}"
-  #   rescue URI::InvalidURIError => e
-  #     puts "Artist name no English #{artist}"
-  #   rescue RestClient::BadRequest => e
-  #     puts "Bad request #{e}"
-  #   rescue => e
-  #     puts "Something went terribly wrong :( #{e}"
-  #   end
-  #   return events if events
-  # end
-
-  @@events = nil
-
   def search_spotify(query)
     puts "Query: #{query}"
     result = nil
     begin
       artists = RSpotify::Artist.search(query)
       found_artist = artists.first
-      if found_artist.popularity < 35
+      if found_artist.popularity < 50
         result = found_artist
         puts "#{result} in the loop"
       end
@@ -97,24 +21,22 @@ module Api
   end
 
 
-  def search_bandsintown(date, city, state)
+  def search_bandsintown(start_date, end_date, city, state)
     found_events = []
 
     begin
-      base_url = "http://api.bandsintown.com/events/search.json?api_version=2.0&app_id=#{ENV['BANDSINTOWN_ID']}&date=#{date}&location=#{city},#{state}"
+      base_url = "http://api.bandsintown.com/events/search.json?api_version=2.0&app_id=#{ENV['BANDSINTOWN_ID']}&date=#{start_date},#{end_date}&location=#{city},#{state}"
       unclean = RestClient.get(base_url)
       events = JSON.parse(unclean)
 
       found_artist = nil
 
       events.each do |event|
+        puts "here"
         spotify_search_result = search_spotify(event['artists'][0]['name'])
         if spotify_search_result != nil && event['ticket_status'] == 'available'
           found_events.push(event)
         end
-        # if found_event
-        #   return found_event
-        # end
       end
     rescue => e
       puts "Something went terribly wrong :( #{e}"
