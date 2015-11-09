85dafdee8e251ab4e235fad79da3584cdc3c3c60
Andrew Haupt <student@epicodus.com>
2015-11-09 12:14:17 -0800

    Can save events to city_dates

--- lib/api.rb
+++ lib/api.rb
@@ -19,30 +19,37 @@ module Api
   end
 
   def search_bandsintown(date, city, state)
+    binding.pry
+    city.downcase!
     found_events = []
+
       begin
         base_url = "http://api.bandsintown.com/events/search.json?api_version=2.0&app_id=#{ENV['BANDSINTOWN_ID']}&date=#{date}&location=#{city},#{state}"
         unclean = RestClient.get(base_url)
         events = JSON.parse(unclean)
 
+        if events
+          if CityDate.find_by(date: date, city: city)
+            city_date = CityDate.find_by(date: date, city: city, state:state)
+          else
+            city_date = CityDate.create(date: date, city: city, state:state)
+          end
+        else
+            puts "Something bad happened man"
+            binding.pry
+          return false
+        end
+
         found_artist = nil
 
         events.each do |event|
-          new_event = Event.new
-          new_event.datetime = DateTime.parse(event['datetime'])
-          new_event.ticket_url = event['ticket_url']
-          new_event.venue_name = event['venue']['name']
-          new_event.venue_lat = event['venue']['latitude']
-          new_event.venue_long = event['venue']['longitude']
-          new_event.save
-
+          new_event = Event.create(datetime:DateTime.parse(event['datetime']), ticket_url:event['ticket_url'], venue_name:event['venue']['name'], venue_lat:event['venue']['latitude'], venue_long:event['venue']['longitude'])
+          city_date.events << new_event
           event['artists'].each do |artist|
           artist['name'].sub!('+', "Plus")
           spotify_search_result = search_spotify(artist['name'])
             if spotify_search_result != nil && event['ticket_status'] == 'available'
-              new_artist = Artist.new
-              new_artist.name = spotify_search_result.name
-              new_artist.song_preview = spotify_search_result.top_tracks(:US).first.preview_url
+              new_artist = Artist.new(name: spotify_search_result.name, song_preview:spotify_search_result.top_tracks(:US).first.preview_url)
               if spotify_search_result.images != []
                 new_artist.image_url = spotify_search_result.images[0]['url']
               elsif spotify_search_result.top_tracks(:US)[0].album.images != []
