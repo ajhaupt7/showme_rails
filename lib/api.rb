d2c6873640f57c4371d42b4959c48c082f3245e9
Andrew Haupt <ajhaupt7@gmail.com>
2015-11-09 08:18:06 -0800

    Mess with timeout

--- lib/api.rb
+++ lib/api.rb
@@ -20,30 +20,29 @@ module Api
 
   def search_bandsintown(date, city, state)
     found_events = []
+      begin
+        base_url = "http://api.bandsintown.com/events/search.json?api_version=2.0&app_id=#{ENV['BANDSINTOWN_ID']}&date=#{date}&location=#{city},#{state}"
+        unclean = RestClient.get(base_url)
+        events = JSON.parse(unclean)
 
-    begin
-      base_url = "http://api.bandsintown.com/events/search.json?api_version=2.0&app_id=#{ENV['BANDSINTOWN_ID']}&date=#{date}&location=#{city},#{state}"
-      unclean = RestClient.get(base_url)
-      events = JSON.parse(unclean)
+        found_artist = nil
 
-      found_artist = nil
-
-      events.each do |event|
-        event['artists'].each do |artist|
-        artist['name'].sub!('+', "Plus")
-        spotify_search_result = search_spotify(artist['name'])
-          if spotify_search_result != nil && event['ticket_status'] == 'available'
-            artist['preview'] = spotify_search_result.top_tracks(:US).first.preview_url
-            if !found_events.include?(event)
-              found_events.push(event)
+        events.each do |event|
+          event['artists'].each do |artist|
+          artist['name'].sub!('+', "Plus")
+          spotify_search_result = search_spotify(artist['name'])
+            if spotify_search_result != nil && event['ticket_status'] == 'available'
+              artist['preview'] = spotify_search_result.top_tracks(:US).first.preview_url
+              if !found_events.include?(event)
+                found_events.push(event)
+              end
             end
           end
         end
+      rescue => e
+        puts "Something went terribly wrong :( #{e}"
+        return false
       end
-    rescue => e
-      puts "Something went terribly wrong :( #{e}"
-      return false
-    end
     return found_events
   end
 end
