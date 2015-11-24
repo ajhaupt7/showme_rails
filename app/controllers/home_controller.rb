dbff45aaf1e049671458e63ea80037a409148078
Andrew Haupt <ajhaupt7@gmail.com>
2015-11-23 17:59:20 -0800

    Rescue errors from home controller

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -8,22 +8,28 @@ class HomeController < ApplicationController
   end
 
   def results
-    params[:city].strip!
-    if CityDate.find_by(date: params[:date], city: params[:city], state:params[:state]) == nil
-      result = search_bandsintown(params[:date], params[:city], params[:state])
-      if result == false
-        flash[:alert] = "We either couldn't find that location or there were no events that day. Try again!"
-        redirect_to root_path
-        return
+    begin
+      params[:city].strip!
+      if CityDate.find_by(date: params[:date], city: params[:city], state:params[:state]) == nil
+        result = search_bandsintown(params[:date], params[:city], params[:state])
+        if result == false
+          flash[:alert] = "We either couldn't find that location or there were no events that day. Try again!"
+          redirect_to root_path
+          return
+        end
       end
-    end
-    @city_date = CityDate.find_by(date: params[:date], city: params[:city], state:params[:state])
-    @events = @city_date.events
-    @artists = []
-    @events.each do |event|
-      event.artists.each do |artist|
-        @artists.push(artist)
+      @city_date = CityDate.find_by(date: params[:date], city: params[:city], state:params[:state])
+      @events = @city_date.events
+      @artists = []
+      @events.each do |event|
+        event.artists.each do |artist|
+          @artists.push(artist)
+        end
       end
+    rescue => e
+      flash[:alert] = "Sorry, something went wrong with your request. Try again!"
+      puts "Error: #{e}"
+      redirect_to root_path
     end
   end
 
